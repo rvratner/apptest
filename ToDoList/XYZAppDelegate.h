f4ed782db74a88e194e01be0240f8d60ecc0bf88
Rachel Ratner <rvratner@rachel-ratners-macbook-2.local>
2014-03-02 19:35:42 -0800

    initial commit from local git

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Rachel Ratner on 3/2/14.
+//  Copyright (c) 2014 Rachel Ratner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Rachel Ratner on 3/2/14.
+//  Copyright (c) 2014 Rachel Ratner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
